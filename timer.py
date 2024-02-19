e428324fd3dcabc796b7628a353f524bd100422b
Atroci <hugomoreiradecarvalho@gmail.com>
2024-02-19 18:12:32 +0000

    foxpile.py

--- timer.py
+++ timer.py
@@ -1,43 +1,79 @@
 import discord
-from discord.ui import Button, View
-from discord.ext import commands, tasks
+from discord.ext import commands
 import asyncio
+from datetime import datetime, timedelta
+import time
 
-TOKEN = 'your_bot_token_here'
-CHANNEL_ID = 123456789012345678  # The channel ID where notifications are sent
+# Define the intents for the bot
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.reactions = True
+intents.message_content = True
 
-class TimerBot(commands.Bot):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.timer_duration = 48 * 3600  # 48 hours in seconds
-        self.warning_time = 12 * 3600  # 12 hours in seconds
-        self.timer_task = None
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-    async def setup_hook(self):
-        self.timer_task = self.loop.create_task(self.timer_loop())
+# Dictionary to keep track of channel expiration timestamps and notification status
+channel_expirations = {}
 
-    async def timer_loop(self):
-        await self.wait_until_ready()
-        channel = self.get_channel(CHANNEL_ID)
-        while not self.is_closed():
-            await asyncio.sleep(self.timer_duration - self.warning_time)
-            await channel.send("@everyone Less than 12 hours remaining!")
-            await asyncio.sleep(self.warning_time)
+async def delete_channel_if_expired(channel_id):
+    """Deletes the channel if the current time is past the end_timestamp and sends a notification 12 hours before."""
+    while True:
+        if channel_id in channel_expirations:
+            end_timestamp = channel_expirations[channel_id]['timestamp']
+            time_left = end_timestamp - time.time()
+            
+            if time_left <= 0:
+                channel = bot.get_channel(channel_id)
+                if channel:
+                    await channel.delete(reason="Time expired for this channel.")
+                del channel_expirations[channel_id]  # Remove the channel from tracking
+                break
+            elif time_left <= 43200 and not channel_expirations[channel_id].get('notified', False):  # 12 hours in seconds
+                channel = bot.get_channel(channel_id)
+                if channel:
+                    await channel.send("@here, this channel will expire in less than 12 hours!")
+                    channel_expirations[channel_id]['notified'] = True
+        await asyncio.sleep(60)  # Sleep for 60 seconds before checking again
 
-    async def reset_timer(self):
-        if self.timer_task:
-            self.timer_task.cancel()
-        self.timer_task = self.loop.create_task(self.timer_loop())
+@bot.command()
+@commands.has_permissions(manage_channels=True)
+async def stockpile(ctx, name: str, role_name: str, code: str):
+    """Creates a channel that's only accessible by a specified role and includes a stockpile code."""
+    if ctx.guild is None:
+        await ctx.send("This command can only be used in a server.")
+        return
 
-bot = TimerBot(command_prefix='!')
+    role = discord.utils.get(ctx.guild.roles, name=role_name)
+    if not role:
+        await ctx.send(f"The role '{role_name}' does not exist.")
+        return
 
-@bot.command(name='reset_timer')
-async def reset_timer(ctx):
-    await bot.reset_timer()
-    await ctx.send("Timer has been reset to 48 hours.")
+    overwrites = {
+        ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        role: discord.PermissionOverwrite(read_messages=True)
+    }
+
+    channel = await ctx.guild.create_text_channel(name, overwrites=overwrites)
+    end_timestamp = time.time() + timedelta(hours=48).total_seconds()
+    channel_expirations[channel.id] = {'timestamp': end_timestamp, 'notified': False}
+
+    discord_timestamp = f"<t:{int(end_timestamp)}:F>"
+    await channel.send(f"Stockpile Code: {code}. This stockpile expires on {discord_timestamp}.")
+
+    asyncio.create_task(delete_channel_if_expired(channel.id))
 
 @bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+async def on_reaction_add(reaction, user):
+    """Refreshes the expiration timer every time a new reaction is added to the initial message."""
+    channel_id = reaction.message.channel.id
+    if user != bot.user and channel_id in channel_expirations:
+        new_end_timestamp = time.time() + timedelta(hours=48).total_seconds()
+        channel_expirations[channel_id]['timestamp'] = new_end_timestamp
+        channel_expirations[channel_id]['notified'] = False  # Reset notification status
+        
+        discord_timestamp = f"<t:{int(new_end_timestamp)}:F>"
+        await reaction.message.channel.send(f"The timer has been refreshed. This stockpile now expires on {discord_timestamp}.")
 
-bot.run(TOKEN)
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
+bot.run('YOUR_BOT_TOKEN')
